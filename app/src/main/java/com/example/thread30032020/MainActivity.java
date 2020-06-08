10706f150c67f34fa89f9ccc119fe06a4ac4007f
phatdroid94 <phatdroid94@gmail.com>
2020-06-08 20:19:26 +0700

    create thread

--- app/src/main/java/com/example/thread30032020/MainActivity.java
+++ app/src/main/java/com/example/thread30032020/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.thread30032020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // background thread
+        final Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("BBB","hello");
+            }
+        });
+
+        thread.start();
+
+        // Kiem tra vong doi cua thread
+        Log.d("BBB",thread.getState().toString());
+
     }
 }
